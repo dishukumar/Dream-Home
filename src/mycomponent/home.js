883eb95eb484aac9e4dee6ac1a9ff0ca553fcd54
deepaksaini2218 <sainimonti17@gmail.com>
2022-08-24 11:04:54 +0530

    import home page

--- src/mycomponent/home.js
+++ src/mycomponent/home.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function home() {
   return (
+    <div>
     <div>home</div>
+    <div>home</div>
+    <div>home</div>
+    <div>home</div>
+    <div>home</div>
+    </div>
   )
 }
